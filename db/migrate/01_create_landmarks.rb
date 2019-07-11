61f87ba4547e92e4a3f36275774988ade39db0f2
Scott Silverman <scottms724@gmail.com>
2019-07-11 18:13:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,11 @@
 class CreateLandmarks < ActiveRecord::Migration
   
+  def change
+    create_table :landmarks do |t|
+      t.string :name 
+      t.string :year_completed 
+      t.string :figure_id 
+    end 
+  end 
+  
 end
